5cd16627a28f39033e326ec35ba0130ca1765227
eric <eric@antlabs.com>
2023-03-08 22:14:45 +0800

    Adding needed class files, Pocket.php is empty for now need to do some thinking

---
+++ src/ClassIterator.php
@@ -0,0 +1,64 @@
+<?php
+namespace Pocket;
+
+/**
+ * Class ClassIterator
+ * @package src\Pocket\ClassIterator
+ * This iterator class will stack the dependencies first before the main class, to be later on read and loaded by the Pocket class
+ */
+class ClassIterator implements \Iterator
+{
+    private $classes = [];
+    private $key = 0;
+
+    public function __construct(string $class)
+    {
+        $this->collectClass($class);
+    }
+
+    private function collectClass(string $class): void
+    {
+        $parameters = [];
+
+        if (method_exists($class, '__construct')) {
+            $reflectionMethod = new \ReflectionMethod($class, '__construct');
+            $parameters = $reflectionMethod->getParameters();
+        }
+
+        foreach ($parameters as $parameter) {
+            if (!$parameter instanceof \ReflectionParameter) continue;
+
+            if (!$parameter->getType()->isBuiltin()) {
+                $this->collectClass($parameter->getType());
+            }
+        }
+
+        // push the class is at the end of this function so that the very end dependency will be first in the stack
+        $this->classes[] = $class;
+    }
+
+    public function current(): mixed
+    {
+        return $this->classes[$this->key];
+    }
+
+    public function rewind(): void
+    {
+        $this->key = 0;
+    }
+
+    public function next(): void
+    {
+        $this->key += 1;
+    }
+
+    public function valid(): bool
+    {
+        return isset($this->classes[$this->key]);
+    }
+
+    public function key(): int
+    {
+        return $this->key;
+    }
+}
