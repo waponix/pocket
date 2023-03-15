0a6d8caa60e20d5e37e9c04dccdb497113e5cc9d
waponix <eric.bermejo.reyes@gmail.com>
2023-03-16 00:35:00 +0800

    Updated the description

--- src/Attribute/Service.php
+++ src/Attribute/Service.php
@@ -4,8 +4,8 @@ namespace Waponix\Pocket\Attribute;
 use Waponix\Pocket\Attribute\Interface\ServiceAttributeInterface;
 /**
  * The Service Attribute:
- * explicitly define argument values to inject into object when it is instantiated, 
- * use @ sign if the value you need is coming from Pocket->parameters (e.g @person.john.name = $parameters['person']['john']['name'])
+ * explicitly define argument values to inject into the object when it is instantiated, 
+ * use @ sign if the value needed is coming from Pocket->parameters (e.g @person.john.name = $parameters['person']['john']['name'])
  */
 #[\Attribute]
 class Service implements ServiceAttributeInterface
